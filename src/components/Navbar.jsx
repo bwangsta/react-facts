d2cf18d2186c2387717709870fb3567bc1719bcd
Brian Wang <bwbrianwang1018@gmail.com>
2022-12-22 18:44:15 -0800

    Finish styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import logo from "../assets/react.svg"
+import logo from "../assets/react-icon-small.png"
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <img src={logo} alt="React Logo"/>
-            <h2>ReactFacts</h2>
-            <p>React Course - Project 1</p>
+            <img src={logo} alt="React Logo" className="nav-logo"/>
+            <h2 className="nav-title">ReactFacts</h2>
+            <p className="nav-course">React Course - Project 1</p>
         </nav>
     )
 }
