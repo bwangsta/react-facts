2d7b801fce3008f1ddbae5405034d6824f350fa8
Brian Wang <bwbrianwang1018@gmail.com>
2022-12-22 16:52:36 -0800

    Finish website markup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,11 @@
+import logo from "../assets/react.svg"
+
 export default function Navbar() {
     return (
-        <h1>Navbar here</h1>
+        <nav className="navbar">
+            <img src={logo} alt="React Logo"/>
+            <h2>ReactFacts</h2>
+            <p>React Course - Project 1</p>
+        </nav>
     )
 }
